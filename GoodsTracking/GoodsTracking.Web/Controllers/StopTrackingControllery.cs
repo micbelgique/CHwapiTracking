0704f03e670c43375a0ec8e70efc7103aad94c90
Di Nicola Emmanuel <emmanuel.dinicola@trasysgroup.com>
2016-08-06 03:29:39 +0200

    Add StopTrackingPage

---
+++ GoodsTracking/GoodsTracking.Web/Controllers/StopTrackingControllery.cs
@@ -0,0 +1,42 @@
+﻿using GoodsTracking.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoodsTracking.Web.Controllers
+{
+    public class StopTrackingController : WebControllerBase
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Add tracker
+        public ActionResult StopTracker(ViewModels.StopTrackingViewModel stopTrackingViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                bool validationResult = true;
+                if (stopTrackingViewModel.ItemIdentifier == null || stopTrackingViewModel.ItemIdentifier.All(s => string.IsNullOrEmpty(s)))
+                {
+                    ModelState.AddModelError(nameof(stopTrackingViewModel.ItemIdentifier), new ValidationException("One item must be filled"));
+                    validationResult = false;
+
+                }
+
+                if (validationResult)
+                {
+                    ModelState.Clear();
+                    return View("~/Views/StopTracking/Index.cshtml");
+                }
+            }
+
+            return View("~/Views/StopTracking/Index.cshtml", stopTrackingViewModel);
+        }
+    }
+}
---
+++ GoodsTracking/GoodsTracking.Web/ViewModels/StopTrackingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoodsTracking.Web.ViewModels
+{
+    public class StopTrackingViewModel
+    {
+        public string ContainerIdentifier { get; set; }
+
+        public string[] ItemIdentifier { get; set; }
+    }
+}
